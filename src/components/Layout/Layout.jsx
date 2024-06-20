45f5efbe8ab50f309178dd3d3f15e897914a954e
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-06-20 21:54:15 +0300

    working on loin form logic and firebase auth

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { Outlet, Navigate } from "react-router";
 
 import './Layout.css'
 
 const Layout = () => {
+
+  const [authenticated, setAuthenticated] = useState(false);
+
+  if (!authenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
   return (
  <>
   <div className="Layout">
-    
+  <nav className="nav-bar">
+    <div className="logo">Bank Logo</div>
+    <ul>
+      <li>Home</li>
+      <li>Add User</li>
+      <li></li>
+    </ul>
+  </nav>
+    <Outlet/>
   </div>;
  </>
   ) 
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -3,7 +3,18 @@ import React from "react";
 import './LoginPage.css'
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <>
+      <div className="LoginPage">
+        <div className="login-form-container">
+          <div className="login-image"></div>
+          <div className="login-form">
+            <h1 className="login-title">LOGIN</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 };
 
 export default LoginPage;

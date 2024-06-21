c7140c349ea3bef6eba4277d87eca987eac16082
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-06-21 12:27:29 +0300

    Hiding sensitive info

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -5,13 +5,13 @@ import { getAuth } from 'firebase/auth';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBF7WpuCnWN1B0tNIp8CGIpn3Q96iXyl98",
-  authDomain: "bankmanagementapi.firebaseapp.com",
-  projectId: "bankmanagementapi",
-  storageBucket: "bankmanagementapi.appspot.com",
-  messagingSenderId: "805092074245",
-  appId: "1:805092074245:web:50c504c5ba16f39e694d6c",
-  measurementId: "G-44HY9R1HHR"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 
